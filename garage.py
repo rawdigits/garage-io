33eb4a863f6d8ed2c760fe5b0420b4e945a7a194
Ryan Huber <rhuber@gmail.com>
2014-11-30 21:35:34 -0800

    rewrote as a state machine

--- garage.py
+++ garage.py
@@ -1,138 +1,125 @@
 #!/usr/bin/python
 
+import time
 import serial
 import redis
-import pynma
-import time
-import os
 
-from config import *
-from shared import *
+SERIAL_PORT='/dev/ttyACM0'
+REDIS_HOST='localhost'
+REDIS_PORT=6379
 
-wait_for_redis()
+#{'status': 'CLOSED', 'motion': 'NOMOTION', 'button': 'UNPRESSED'} TEMPDISARMED
+#inputs
+#door sensor, motion sensor, button pressed, armed
 
-ser = serial.Serial(SERIAL_PORT,9600,timeout=5)
-r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=0)
-p = pynma.PyNMA( r.get('prowl-api-key') )
-ser.flushInput()
+class Sensors(object):
+    def __init__(self):
+        self.sensors = []
+        self.debug = False
+    def find(self, values):
+        for value in values:
+            if value not in self.sensors:
+                return False
+        return True
+    def update_sensors(self):
+        #sensors = 'OPEN:NOMOTION:UNPRESSED'.split(':')
+        #sensors.append('ARMED')
+        sensors = ser.readline().strip().split(':')
+	sensors.append(r.get('security-mode').strip())
+        self.sensors = sensors
+	#print self.sensors
+    def check(self, values):
+        if not self.debug:
+            self.update_sensors()
+        return self.find(values)
+    def debug_set(self, values):
+        self.debug = True
+        self.sensors = values
 
-def send_message(message):
-  try:
-    p.push("GARAGE","GARAGE",message)
-  except:
+class Garage(object):
+    def __init__(self):
+        self.state = 'open'
+        self.sensors = Sensors()
+        self.timer = 0
+    def transition(self):
+        command = r.get("command")
+        if command.startswith("APPOPEN"):
+	    milliseconds = command.split(":")[1]
+            set_disarmed()
+            toggle_relay(milliseconds)
+            clear_command()
+
+        if self.state == "closed":
+            if self.sensors.check(["OPEN", "DISARM"]):
+                self.state = "open"
+            elif self.sensors.check(["DISARM", "PRESSED"]):
+                print "PRESSED"
+                toggle_relay()
+            elif self.sensors.check(["ARMED", "PRESSED"]):
+                self.state = "alarming"
+            elif self.sensors.check(["ARMED", "MOTION"]):
+                self.state = "alarming"
+            elif self.sensors.check(["ARMED", "OPEN"]):
+                self.state = "alarming"
+        elif self.state == "open":
+            if self.sensors.check(["CLOSED"]):
+                self.timer = int(time.time())
+                self.state = "arming"
+            elif self.sensors.check(["MOTION", "ARMED"]):
+                self.state = "alarming"
+            elif self.sensors.check(["PRESSED", "ARMED"]):
+                self.state = "alarming"
+            elif self.sensors.check(["PRESSED", "DISARM"]):
+                toggle_relay()
+        elif self.state == "arming":
+            if self.sensors.check(["MOTION", "CLOSED"]):
+                set_disarmed()
+                self.state = "closed"
+            elif self.timer + 15 < int(time.time()) and self.sensors.check(["CLOSED"]):
+                set_armed()
+                self.state = "closed"
+            elif self.sensors.check(["OPEN"]):
+                set_disarmed()
+                self.state = "open"
+            elif self.sensors.check(["PRESSED"]):
+                toggle_relay()
+        elif self.state == "timed_opening":
+            if self.sensors.check(["DISARM"]):
+                self.state = "open"
+        elif self.state == "alarming":
+            if self.sensors.check(["DISARM"]):
+                self.state = "open"
+
+def clear_command():
+    r.set('command', "")
+
+def set_armed():
+    r.set('security-mode', "ARMED")
+    print "armed"
     pass
 
-class Alerter:
-  def __init__(self):
-    self.last_alert = 0
+def set_disarmed():
+    r.set('security-mode', "DISARM")
+    print "disarmed"
+    pass
 
-  def alert(self, message):
-    if time.time() > (self.last_alert + 10):
-      for i in range(1,5):
-        send_message(message)
-        time.sleep(.5)
-        os.system('/usr/bin/aplay /home/pi/alarm.wav')
-      ser.flushInput()
-      self.last_alert = time.time()
-
-alerter = Alerter()
-
-def toggle_relay(now = None):
-  if now:
-    security_mode = "TEMPDISARMED"
-    r.set('security-mode', security_mode)
-    ser.write("OPEN:0" + "!")
-    time.sleep(1)
-  else:
-    command = r.get('command')
-    print "%s : %s" % (time.asctime(), command)
-    if command != None and command[0:4] == "OPEN":
-      security_mode = "TEMPDISARMED"
-      r.set('security-mode', security_mode)
-      ser.write(command + "!")
-      r.set('command','')
-      time.sleep(.5)
-      ser.flushInput()
-
-def check_sensors():
-  try:
-    state_in = ser.readline().strip().split(':')
-    state = {"status":state_in[0], "motion":state_in[1], "button":state_in[2]}
-  except:
-    state = {"status":"CLOSED", "motion":"NOMOTION", "button":"UNPRESSED"}
-  hard_button.monitor(state["button"])
-  r.set('security-status', "%s" % (state))
-  return state
-
-def check_mode():
-  try:
-    mode = r.get('security-mode').strip()
-  except:
-    mode = "ARMED"
-  return mode
-
-class Button:
-  def __init__(self):
-    self.last_state = ""
-    self.checked = True
-  def check(self):
-    print "called check, checked: %s" % (self.checked)
-    if self.checked == False:
-      self.checked = True
-      return True
+def toggle_relay(milliseconds=None):
+    if milliseconds:
+        ser.write("OPEN:{}!".format(milliseconds))
     else:
-      return False
-  def monitor(self, state):
-    if state != self.last_state and state == "PRESSED":
-      self.checked = False
-    self.last_state = state
+        ser.write("OPEN!")
+    print "relay toggled"
+    pass
 
-hard_button = Button()
-
-
-#YES I KNOW THIS NEEDS TO BECOME A STATE MACHINE
-while True:
-  state = check_sensors()
-  toggle_relay()
-  mode = check_mode()
-  #This allows a TEMP DISARM mode, which means disable until we are 'CLOSED' again
-  if mode == 'TEMPDISARMED':
-    if hard_button.check():
-      toggle_relay(True)
-    if state["status"] == 'OPEN':
-      while True:
-        if hard_button.check():
-          toggle_relay(True)
-        state = check_sensors()
-        toggle_relay()
-        #allows you to arm motion sensor without closing door and disarm to break the loop
-        mode = check_mode()
-        if mode == "ARMED" and state["motion"] == "MOTION":
-          alerter.alert("%s" % (state))
-        elif mode == "DISARM":
-          break
-        if state["status"] == 'CLOSED':
-          send_message("Door Closed")
-          #First if we see movement someone is presumably inside intentionally
-          security_mode = "ARMED"
-          closed_time = time.time()
-          while time.time() < closed_time + 15:
-            state = check_sensors()
-            if state["motion"] == "MOTION":
-              security_mode = "TEMPDISARMED"
-          #Otherwise arm the system
-          r.set('security-mode', security_mode)
-          send_message(security_mode)
-          break
-    else:
-      continue
-
-  if (state["status"] != 'CLOSED' or state["motion"] == 'MOTION') and mode == 'ARMED':
-    alerter.alert("%s" % (state))
-  elif mode == 'TEST':
-    r.set('security-mode', '')
-    alerter.alert("%s" % (state))
-  else:
-    #this below line makes sure a tempdisarm doesn't open the door accidentally
-    hard_button.check()
-
+if __name__ == "__main__":
+    garage = Garage()
+    prev_state = ""
+    ser = serial.Serial(SERIAL_PORT,9600,timeout=5)
+    ser.flushInput()
+    r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=0)
+    while True:
+        garage.transition()
+        if garage.state != prev_state:
+            print garage.state
+        prev_state = garage.state
+    pass
